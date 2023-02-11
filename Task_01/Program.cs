dcd11d04dc1ddf076b8f116da9e08f2484c38be7
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-11 17:08:30 +0300

    task_01 correct homework added

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -11,11 +11,15 @@ if (a > b)
 {
     Console.Write("max = ");
     Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
 }
 else 
 {
     Console.Write("max = ");
     Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.WriteLine(a);
 }
 
 
